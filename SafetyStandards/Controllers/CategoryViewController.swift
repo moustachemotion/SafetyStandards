28e298cc538a8520765744d73f387cd5ba20c430
Daryl Jacob <daryl@moustachemotion.com>
2017-12-31 00:11:03 -0500

    Add super class SwipeTableViewController and apply to CategoryViewController

--- SafetyStandards/Controllers/CategoryViewController.swift
+++ SafetyStandards/Controllers/CategoryViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class CategoryViewController: UITableViewController {
+class CategoryViewController: SwipeTableViewController {
 
     let realm = try! Realm()
     
@@ -21,8 +21,11 @@ class CategoryViewController: UITableViewController {
 
         loadCategories()
         
+        tableView.rowHeight = 80.0
+        
     }
 
+    //MARK: - Add New Categories
     @IBAction func addButtonPressed(_ sender: Any) {
         
         var textField = UITextField()
@@ -62,9 +65,10 @@ class CategoryViewController: UITableViewController {
         return categories?.count ?? 1
     }
     
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
         
@@ -110,6 +114,19 @@ class CategoryViewController: UITableViewController {
         
         tableView.reloadData()
         
+    }
+    //MARK: - Delete Data From Swipe
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let categoryForDeletion = self.categories?[indexPath.row] {
+            do {
+                try self.realm.write {
+                    self.realm.delete(categoryForDeletion)
+                }
+            } catch {
+                print("Error deleting category, \(error)")
+            }
+        }
     }
     
 }
